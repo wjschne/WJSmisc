0fc102271df13efc320c62723721601a5ba1c41a
W. Joel Schneider <w.joel.schneider@gmail.com>
2019-09-29 07:12:56 -0400

    fix tidyeval errors

--- R/main.R
+++ R/main.R
@@ -1,11 +1,13 @@
 
 #' Function that converts pdf files to svg. Must have the pdf2svg program installed (https://github.com/dawbarton/pdf2svg)
+#'
 #' @export
 #' @param s Filename of the pdf file. Omit the ".pdf" at the end.
 pdf2svg <- function(s) {shell(paste0("pdf2svg ",s,".pdf ",s,".svg"))}
 
 
 #' Computes covariances of composite scores given a covariance matrix and a weight matrix
+#'
 #' @export
 #' @param Sigma Covariance matrix
 #' @param w Weight matrix. Must have the same number of rows as R
@@ -21,10 +23,12 @@ pdf2svg <- function(s) {shell(paste0("pdf2svg ",s,".pdf ",s,".svg"))}
 #' composite_covariance(Sigma, w)
 composite_covariance <- function(Sigma,w, correlation = FALSE) {
   Sigma_composite <- t(w) %*% Sigma %*% w
-  if (correlation) cov2cor(Sigma_composite) else Sigma_composite
+  if (correlation) stats::cov2cor(Sigma_composite) else Sigma_composite
   }
 
 #' Plot a normal distribution shading below x
+#'
+#' @export
 #' @param x number to divide normal distribution
 #' @param mu mean of normal distribution
 #' @param sigma standard deviation of normal distribution
@@ -35,7 +39,7 @@ composite_covariance <- function(Sigma,w, correlation = FALSE) {
 #' @param text_size Base text size
 #' @param shade_fill Color of shaded region
 #' @import ggplot2
-#' @example
+#' @examples
 #' plotnorm(90, 100, 15)
 plotnorm <- function(x,mu,sigma, below = TRUE, show_proportion = TRUE, show_x = TRUE, show_param = TRUE, text_size = 14, shade_fill = "royalblue") {
   dx <- round(x,2)
@@ -130,4 +134,160 @@ plotnorm <- function(x,mu,sigma, below = TRUE, show_proportion = TRUE, show_x = 
   p
 }
 
+
+
+
+#' Creats a plot of factor loadings
+#'
+#' @export
+#' @param f output of a factor analysis from the psych::fa function
+#' @param factor_names names of the factors #' @import patchwork
+#' @import ggplot2
+#' @import patchwork
+#' @importFrom rlang .data
+#' @examples
+#' library(psych)
+#' fit <- fa(psych::bfi[,1:25], nfactors = 5)
+#' plot_loading(fit)
+plot_loading <- function(f, factor_names = sort(colnames(f$loadings))) {
+  factor_names <- stringr::str_replace_all(factor_names, "_", "\n")
+
+  d_factor_names <- tibble::tibble(Factor = colnames(f$loadings), Factor_labels = factor_names) %>%
+    dplyr::mutate(Factor_labels = forcats::fct_inorder(.data$Factor_labels))
+  p1 <- f %>%
+    psych::fa.sort() %>%
+    `[[`("loadings") %>%
+    unclass() %>%
+    tibble::as_tibble(rownames = "Test") %>%
+    dplyr::mutate(Test = forcats::fct_infreq(.data$Test) %>% forcats::fct_rev()) %>%
+    tidyr::gather(key = "Factor", value = "Loadings", -.data$Test) %>%
+    dplyr::mutate(Test = forcats::fct_inorder(.data$Test) %>% forcats::fct_rev()) %>%
+    dplyr::arrange(.data$Test) %>%
+    dplyr::left_join(d_factor_names, by = "Factor") %>%
+    ggplot(aes(.data$Factor_labels, .data$Test)) +
+    geom_tile(aes(fill = .data$Loadings)) +
+    geom_text(aes(label = formatC(.data$Loadings, 2, format = "f") %>%
+                    stringr::str_remove("^0") %>%
+                    stringr::str_replace("^-0", "-")),
+              family = "serif") +
+    scale_fill_gradient2(low = "firebrick", mid = "white", high = "royalblue", midpoint = 0, limits = c(-1,1)) +
+    theme_minimal(base_family = "serif", base_size = 14) +
+    labs(x = NULL, y = NULL) +
+    theme(legend.position = "none") +
+    ggtitle(paste0("Factor Loadings", ifelse(any(abs(f$loadings) > 1), " (Heywood case detected)","")))
+
+  if (f$factors == 1) p1 else {
+    p2 <-  corrr::as_cordf(f$Phi, diagonal = 1) %>%
+      dplyr::rename(Factor = .data$rowname) %>%
+      tidyr::gather(key = "f", value = "r", -.data$Factor) %>%
+      dplyr::left_join(d_factor_names, by = "Factor") %>%
+      dplyr::mutate(Factor_col = forcats::fct_rev(.data$Factor_labels)) %>%
+      dplyr::select(-.data$Factor_labels) %>%
+      dplyr::left_join(
+        d_factor_names %>%
+          dplyr::rename(f = .data$Factor), by = "f") %>%
+      dplyr::rename(Factor_row = .data$Factor_labels) %>%
+      dplyr::mutate(Factor_col = forcats::fct_relabel(.data$Factor_col, stringr::str_replace_all, pattern = "\n", " ")) %>%
+      ggplot(aes(.data$Factor_row, .data$Factor_col)) +
+      geom_tile(aes(fill = .data$r)) +
+      geom_text(aes(label = formatC(.data$r, 2, format = "f") %>%
+                      stringr::str_remove("^0") %>%
+                      stringr::str_replace("^-0", "-")),
+                family = "serif") +
+      scale_fill_gradient2(
+        low = "firebrick",
+        mid = "white",
+        high = "royalblue",
+        midpoint = 0,
+        limits = c(-1,1)) +
+      theme_minimal() +
+      theme(legend.position = "none") +
+      labs(x = NULL, y = NULL) +
+      ggtitle("Factor Correlations")
+
+
+    p1 + p2 + patchwork::plot_layout(ncol = 1, heights = c(nrow(f$loadings) / ncol(f$loadings), 1))
+  }
+
+
+}
+
+
+
+#' Format text of a correlation matrix
+#'
+#' @export
+#' @param r a matrix of correlations
+#' @param digits Number of digits to round
+#' @param remove_leading_zero If TRUE, remove leading zero from all correlations
+#' @param remove_diagonal If TRUE, remove diagonal of ones
+#' @return a matrix of correlations as text
+#'
+#' @examples
+#' cor_text(matrix(.5,2,2))
+cor_text <- function(
+  r,
+  digits = 2,
+  remove_leading_zero = TRUE,
+  remove_diagonal = FALSE) {
+  rho <- formatC(r, digits = digits, format = "f")
+
+  if (remove_leading_zero) {
+    space_replace <- ifelse(any(r < 0)," ","")
+    rho <- apply(rho, MARGIN = c(1,2), stringr::str_replace, pattern = "^0", replacement = space_replace)
+    rho <- apply(rho, MARGIN = c(1,2), stringr::str_replace, pattern = "^-0", replacement = "-")
+  }
+
+  if (remove_diagonal) {
+    diag(rho) <- ""
+  } else {
+    diag(rho) <- "1"
+  }
+rho
+}
+
+
+
+# Correlation plot
+#' Title
+#'
+#' @export
+#' @param d Data
+#' @param test_names Vector of names of variables
+#' @param margins Width of margins for labels
+#' @param text_size Size of text
+#' @param ... Additional parameters passed to superheat::superheat
+cor_heat <- function(
+  d,
+  test_names = colnames(d),
+  margins = 0.55,
+  text_size = 4,
+  ...) {
+  r <- stats::cor(d, use = "pairwise")
+
+
+
+  dimnames(r) <- list(test_names, test_names)
+
+  superheat::superheat(
+    X = r,
+    X.text = cor_text(r),
+    X.text.size = text_size,
+    col.dendrogram = T,
+    row.dendrogram = T,
+    smooth.heat = F,
+    heat.pal = c("firebrick", "white", "royalblue"),
+    heat.lim = c(-1, 1),
+    heat.pal.values = c(0, .5, 1),
+    legend = F,
+    bottom.label.text.angle = 90,
+    heat.na.col = "white",
+    bottom.label.size = margins,
+    left.label.size = margins,
+    left.label.text.size = text_size,
+    bottom.label.text.size = text_size,
+    padding = 0,
+    ...
+  )
+}
 
